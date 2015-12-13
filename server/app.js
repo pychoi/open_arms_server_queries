339d3360acafb68039b538340db885e17bbeca2c
mmpyhmm <choipui@gmail.com>
2015-12-12 21:08:16 -0600

    complete routes for meal count, get meals, create menu GET, and get menu

--- server/app.js
+++ server/app.js
@@ -4,9 +4,15 @@ var app = express();
 var index = require('./routes/index');
 var getCategories = require('./routes/getcategories');
 var getMealCount = require('./routes/getmealcount');
+var getMeals = require('./routes/getmeals');
+var createMenu = require('./routes/createmenu');
+var getMenu = require('./routes/getmenu');
 
 app.set("port", process.env.PORT || 5000);
 
+app.use('/getmeals', getMeals);
+app.use('/createmenu', createMenu);
+app.use('/getmenu', getMenu);
 app.use('/getcategories', getCategories);
 app.use('/getmealcount', getMealCount);
 app.use('/', index);
